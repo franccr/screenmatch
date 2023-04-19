62ee0e8517ba8bdefca730e0b8db56b37547b28e
Cristiano França <cristiano.st.franca@gmail.com>
2023-04-19 16:24:06 -0300

    Projeto ScreenMatch

---
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -0,0 +1,13 @@
+package br.com.alura.screenmatch.modelos;
+
+public class Filme extends TItulo{
+    private String diretor;
+
+    public String getDiretor() {
+        return diretor;
+    }
+
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
+    }
+}
