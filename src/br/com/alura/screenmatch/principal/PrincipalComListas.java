56c00ec1d98f1171826f86d967f64557b8e1f264
Cristiano França <cristiano.st.franca@gmail.com>
2023-04-25 20:28:30 -0300

    Criação classe PrincipalComListas.java

---
+++ src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -0,0 +1,32 @@
+package br.com.alura.screenmatch.principal;
+
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+import br.com.alura.screenmatch.modelos.TItulo;
+
+import java.util.ArrayList;
+
+public class PrincipalComListas {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme("O poderoso chefão",1970);
+        meuFilme.avalia(9);
+        Serie lost = new Serie("Lost",2000);
+        Filme outroFilme = new Filme("Avatar",2023);
+        outroFilme.avalia(6);
+        var filmeDoPaulo = new Filme("Dogville",2003);
+        filmeDoPaulo.avalia(10);
+
+        ArrayList<TItulo> lista = new ArrayList<>();
+        lista.add(filmeDoPaulo);
+        lista.add(meuFilme);
+        lista.add(outroFilme);
+        lista.add(lost);
+
+        for (TItulo item: lista) {
+            System.out.println(item.getNome());
+            if (item instanceof Filme filme && filme.getClassificacao() > 2){
+                System.out.println("Classificação: " + filme.getClassificacao());
+            }
+        }
+    }
+}
